cc9fcd0aef6b07224b43218d976099682225d940
maiyo008 <tonymaiyo008@gmail.com>
2023-07-23 20:47:08 +0300

    Parsing a random HTTP header from stdin

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,60 +1,48 @@
 #!/usr/bin/python3
-"""
-Log parsing
-"""
+"""script that reads stdin line by line and computes metrics:m"""
 import sys
-from collections import defaultdict
 
 
-def main():
-    """
-    The main program runs from here
-    """
-    file_sizes = []
-    status_counts = defaultdict(int)
-    lines_processed = 0
-    try:
-        for line in sys.stdin:
-            line = line.strip()
-            parsed_line = parse_line(line)
-            if parsed_line:
-                status_code, file_size = parsed_line
-                file_sizes.append(file_size)
-                status_counts[status_code] += 1
-                lines_processed += 1
-                if lines_processed % 10 == 0:
-                    print_statistics(file_sizes, status_counts)
-    except KeyboardInterrupt:
-        print_statistics(file_sizes, status_counts)
+# store the count of all status codes in a dictionary
+status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+                     '404': 0, '405': 0, '500': 0}
 
+total_size = 0
+count = 0  # keep count of the number lines counted
 
-def parse_line(line):
-    """
-    Parses the input from stdin
-    """
-    parts = line.split(' ')
-    if len(parts) != 9 or parts[7]\
-            not in ['200', '301', '400', '401', '403', '404', '405', '500']:
-        return None
-    try:
-        file_size = int(parts[8])
-    except ValueError:
-        return None
-    return parts[7], file_size
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
 
+        if len(line_list) > 4:
+            status_code = line_list[-2]
+            file_size = int(line_list[-1])
 
-def print_statistics(file_sizes, status_counts):
-    """
-    Prints the statistics after every 10 lines,
-    or after a keyboard interrupt
-    """
-    total_size = sum(file_sizes)
-    print("File size: {}".format(total_size))
-    sorted_status_codes = sorted(status_counts.keys())
-    for status_code in sorted_status_codes:
-        count = status_counts[status_code]
-        print("{}: {}".format(status_code, count))
+            # check if the status code receive exists in the dictionary and
+            # increment its count
+            if status_code in status_codes_dict.keys():
+                status_codes_dict[status_code] += 1
 
+            # update total size
+            total_size += file_size
 
-if __name__ == "__main__":
-    main()
+            # update count of lines
+            count += 1
+
+        if count == 10:
+            count = 0  # reset count
+            print('File size: {}'.format(total_size))
+
+            # print out status code counts
+            for key, value in sorted(status_codes_dict.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(status_codes_dict.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
