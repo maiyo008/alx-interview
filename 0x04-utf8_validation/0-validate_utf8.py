8874a9b966f38b81a7ac99d7431731340cc1257f
maiyo008 <tonymaiyo008@gmail.com>
2023-07-25 09:38:18 +0300

    Handle more than 1 byte UTF-8 while only considering the least 8 significant bits

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,15 +12,20 @@ def validUTF8(data):
         bin_rep = bin(d)[2:]
         if d >= 0 and d < 128:
             if bin_rep[0] == 0 or len(bin_rep) < 8:
-                print("UTF-8, 1 byte")
+                # print("UTF-8, 1 byte")
                 isUTF.append(True)
             else:
                 isUTF.append(False)
         if d >= 128 and d < 2048:
-            
-        else:
-            isUTF.append(False)
+            l_significant = bin_rep[-8:]
+            if l_significant[0] == 1 and l_significant[1] == 0:
+                # print("UTF-8, 2,3,4 bytes")
+                isUTF.append(True)
+            else:
+                isUTF.append(False)
     if False not in isUTF:
+        # print(isUTF)
         return True
     else:
+        # print(isUTF)
         return False
